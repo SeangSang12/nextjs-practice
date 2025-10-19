26183e7929e5a8b5a981c62f8ef095e167ded4af
seangsang <sangseang23@gmail.com>
2025-10-19 20:20:49 +0700

    this is route in nextjs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,11 @@
-export default function about(){
-    return <h1>Welcome to about page</h1>
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-3xl">About Page</h1>
+    </div>
+  )
 }
+
+export default page
